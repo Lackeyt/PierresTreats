739e8e14d904ec9aa3e88346be5dfd9c14b227f3
Tyson Lackey <lackeyt90@gmail.com>
2020-08-14 12:23:35 -0700

    update flavor views and model, remirate

---
+++ PierresTreats/Migrations/20200814192305_flavorName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PierresTreats.Migrations
+{
+    public partial class flavorName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Flavors",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Flavors",
+                newName: "Description");
+        }
+    }
+}
--- PierresTreats/Models/Flavor.cs
+++ PierresTreats/Models/Flavor.cs
@@ -9,7 +9,7 @@ namespace PierresTreats.Models
         this.Treats = new HashSet<TreatFlavor>();
     }
     public int FlavorId {get;set;}
-    public string Description {get;set;}
+    public string Name {get;set;}
     public virtual ApplicationUser User {get;set;}
     public ICollection<TreatFlavor> Treats {get;}
   }
